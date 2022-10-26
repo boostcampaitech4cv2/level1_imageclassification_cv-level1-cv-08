daf50d47410527eaf5e44fb529cb6acfece83d0c
juheon <ojhgold@gmail.com>
2022-10-27 00:47:55 +0900

    feat: Add base submit

--- test.py
+++ test.py
@@ -1,81 +1,94 @@
 import argparse
+import collections
+from datetime import datetime
 import torch
 from tqdm import tqdm
+import pandas as pd
 import data_loader.data_loaders as module_data
-import model.loss as module_loss
-import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
 
 
-def main(config):
-    logger = config.get_logger('test')
+def main(CONFIG):
+    logger = CONFIG.get_logger("test")
 
     # setup data_loader instances
-    data_loader = getattr(module_data, config['data_loader']['type'])(
-        config['data_loader']['args']['data_dir'],
-        batch_size=512,
-        shuffle=False,
-        validation_split=0.0,
-        training=False,
-        num_workers=2
+    data_loader = getattr(module_data, CONFIG["data_loader"]["type"])(
+        stage="eval",
+        input_size=CONFIG["data_loader"]["args"]["input_size"],
+        batch_size=CONFIG["data_loader"]["args"]["batch_size"],
+        num_workers=CONFIG["data_loader"]["args"]["num_workers"],
     )
 
     # build model architecture
-    model = config.init_obj('arch', module_arch)
+    model = CONFIG.init_obj("arch", module_arch)
     logger.info(model)
 
-    # get function handles of loss and metrics
-    loss_fn = getattr(module_loss, config['loss'])
-    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
-
-    logger.info('Loading checkpoint: {} ...'.format(config.resume))
-    checkpoint = torch.load(config.resume)
-    state_dict = checkpoint['state_dict']
-    if config['n_gpu'] > 1:
+    logger.info(f"Loading checkpoint: {CONFIG.resume} ...")
+    checkpoint = torch.load(CONFIG.resume)
+    state_dict = checkpoint["state_dict"]
+    if CONFIG["n_gpu"] > 1:
         model = torch.nn.DataParallel(model)
     model.load_state_dict(state_dict)
+    print("model loaded")
 
     # prepare model for testing
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     model.eval()
-
-    total_loss = 0.0
-    total_metrics = torch.zeros(len(metric_fns))
-
+    print("Testing...")
+    preds = []
+    progress = tqdm(data_loader, total=len(data_loader), desc="Testing... ", ncols=200)
     with torch.no_grad():
-        for i, (data, target) in enumerate(tqdm(data_loader)):
-            data, target = data.to(device), target.to(device)
+        for data in progress:
+            data = data.to(device)
             output = model(data)
+            pred = torch.argmax(output, dim=1)
+            preds.extend(pred.detach().cpu().numpy())
 
             #
             # save sample images, or do something with output here
             #
+    print("Testing done.")
 
-            # computing loss, metrics on test set
-            loss = loss_fn(output, target)
-            batch_size = data.shape[0]
-            total_loss += loss.item() * batch_size
-            for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
-
-    n_samples = len(data_loader.sampler)
-    log = {'loss': total_loss / n_samples}
-    log.update({
-        met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
-    })
-    logger.info(log)
+    submit = pd.read_csv("/opt/ml/input/data/eval/info.csv")
+    submit["ans"] = preds
+    now = datetime.now().strftime(r"%m.%d_%H:%M:%S")
+    path_submit = f"/opt/ml/level1_imageclassification_cv-level1-cv-08/submit/{now}.csv"
+    submit.to_csv(path_submit, index=False)
+    print(f"Submit saved to {path_submit}")
 
 
-if __name__ == '__main__':
-    args = argparse.ArgumentParser(description='PyTorch Template')
-    args.add_argument('-c', '--config', default=None, type=str,
-                      help='config file path (default: None)')
-    args.add_argument('-r', '--resume', default=None, type=str,
-                      help='path to latest checkpoint (default: None)')
-    args.add_argument('-d', '--device', default=None, type=str,
-                      help='indices of GPUs to enable (default: all)')
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(description="PyTorch Template")
+    args.add_argument(
+        "-c",
+        "--config",
+        default="config.json",
+        type=str,
+        help="config file path (default: None)",
+    )
+    args.add_argument(
+        "-r",
+        "--resume",
+        default="saved/models/Mask_base/1026_172102/model_best.pth",
+        type=str,
+        help="path to latest checkpoint (default: None)",
+    )
+    args.add_argument(
+        "-d",
+        "--device",
+        default=None,
+        type=str,
+        help="indices of GPUs to enable (default: all)",
+    )
+    # custom cli options to modify configuration from default values given in json file.
+    CustomArgs = collections.namedtuple("CustomArgs", "flags type target")
+    options = [
+        CustomArgs(
+            ["--dtld", "--data_loader"], type=str, target="data_loader;args;stage"
+        ),
+    ]
 
-    config = ConfigParser.from_args(args)
+    config = ConfigParser.from_args(args, options)
     main(config)
