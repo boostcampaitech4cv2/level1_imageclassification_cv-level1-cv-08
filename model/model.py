e2b9d867747d5cd5bfe6017d2219ca90288140ac
juheon <ojhgold@gmail.com>
2022-10-27 19:54:32 +0900

    feat: add torchvision model

--- model/model.py
+++ model/model.py
@@ -1,26 +1,6 @@
 import torch.nn as nn
-import torch.nn.functional as F
 import timm
-from base import BaseModel
-
-
-class CustomModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+from torchvision import models
 
 
 class TimmModel(nn.Module):
@@ -31,6 +11,19 @@ class TimmModel(nn.Module):
         self.model = timm.create_model(model_name, pretrained=pretrained)
         n_features = self.model.classifier.in_features
         self.model.classifier = nn.Linear(n_features, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+class TorchVisionModel(nn.Module):
+    def __init__(self, model_name="efficientnet_v2_s", pretrained=True, num_classes=18):
+        super().__init__()
+
+        self.model = getattr(models, model_name)(pretrained=pretrained)
+        n_features = self.model.classifier[1].in_features
+        self.model.classifier[1] = nn.Linear(n_features, num_classes)
 
     def forward(self, x):
         x = self.model(x)
