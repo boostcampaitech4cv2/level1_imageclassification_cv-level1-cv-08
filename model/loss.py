d0fdc7bb708e0e2ef361fbfa32a98ffdd0ec6f37
Alias26 <khmjuhee979@gmail.com>
2022-11-02 09:45:19 +0000

    fix:loss

--- model/loss.py
+++ model/loss.py
@@ -1,4 +1,6 @@
 import torch.nn.functional as F
+import torch.nn as nn
+import torch
 from pytorch_metric_learning import losses
 
 
@@ -13,6 +15,15 @@ def multilabel_loss(outputs, target):
         weight=torch.tensor(([2.8723, 3.9589, 4.5075, 14.0625, 14.0625, 28.4211]))
     ).cuda()
 
+    mask_out, gender_out, age_out = outputs
+    mask_target, gender_target, age_target = target
+
+    return (
+        0.375 * loss_mask(mask_out, mask_target)
+        + 0.25 * loss_gender(gender_out, gender_target)
+        + 0.375 * loss_age(age_out, age_target)
+    )
+
 
 def ce_loss(output, target):
     return F.cross_entropy(output, target)
@@ -23,5 +34,7 @@ def all_loss(loss_name, output, target):
         return F.nll_loss(output, target)
     elif loss_name == "ce_loss":
         return F.cross_entropy(output, target)
+    elif loss_name == "multilabel_loss":
+        return multilabel_loss(output, target)
     else:
         return getattr(losses, loss_name)()(output, target)
