a23b21905d1ca780b94c1a0fd8774d172104a656
juheon <ojhgold@gmail.com>
2022-11-02 11:12:21 +0900

    add: hard augmentations

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -104,10 +104,28 @@ def setup(
 ):
     train_transform = Compose(
         [
-            # A.CenterCrop(356, 356, p=0.5),
-            # A.Affine(),
             A.Resize(input_size, input_size),
+            A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=5, p=0.5),
+            A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5),
             A.HorizontalFlip(p=0.5),
+            A.OneOf(
+                [
+                    A.MotionBlur(blur_limit=3, p=0.7),
+                    A.MedianBlur(blur_limit=3, p=0.7),
+                    A.GaussianBlur(blur_limit=3, p=0.7),
+                    A.GaussNoise(var_limit=(3.0, 9.0), p=0.7),
+                ],
+                p=0.5,
+            ),
+            A.CoarseDropout(
+                max_holes=10,
+                max_height=20,
+                max_width=20,
+                min_holes=1,
+                min_height=3,
+                min_width=3,
+                p=0.5,
+            ),
             A.Normalize(),
             ToTensorV2(),
         ]
