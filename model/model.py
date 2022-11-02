1276d3ca31d58396bf825bf3be18961820d0c357
GeonHyeock <gunheock@gmail.com>
2022-11-02 02:39:34 +0000

    multilabel model

--- model/model.py
+++ model/model.py
@@ -28,3 +28,38 @@ class TorchVisionModel(nn.Module):
     def forward(self, x):
         x = self.model(x)
         return x
+
+
+class TimmModelMulti(nn.Module):
+    def __init__(self, model_name="efficientnetv2_rw_s", pretrained=True):
+        super().__init__()
+        self.model = timm.create_model(model_name, pretrained=pretrained)
+        self.model.classifier = nn.Sequential(
+            nn.MaxPool1d(4, stride=4), MultiClassFc(448)
+        )
+
+    def forward(self, x):
+        mask, gender, age = self.model(x)
+        return mask, gender, age
+
+
+class MultiClassFc(nn.Module):
+    def __init__(self, backbone_dim):
+        super().__init__()
+        self.mask_out = self.make_out_layer(backbone_dim, 3)
+        self.gender_out = self.make_out_layer(backbone_dim, 2)
+        self.age_out = self.make_out_layer(backbone_dim, 3)
+
+    def forward(self, x):
+        mask_out = self.mask_out(x)
+        gender_out = self.gender_out(x)
+        age_out = self.age_out(x)
+        return mask_out, gender_out, age_out
+
+    def make_out_layer(self, backbone_dim, num_class):
+        return nn.Sequential(
+            nn.Linear(backbone_dim, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, num_class),
+        )
