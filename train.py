17d2281bb616f894a38342eab85da567a2469dc7
Alias26 <80751414+alias26@users.noreply.github.com>
2022-11-03 10:24:53 +0900

    Update train.py

--- train.py
+++ train.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     args.add_argument(
         "-c",
         "--config",
-        default="/opt/ml/level1_imageclassification_cv-level1-cv-08/config.json",
+        default="config.json",
         type=str,
         help="config file path (default: None)",
     )
